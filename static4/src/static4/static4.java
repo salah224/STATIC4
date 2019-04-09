5974453c54b46e7e9abfa1ab2afd150871263db9
salah224 <salah224@hotmail.com>
2019-04-08 20:48:23 -0500

    STATIC4

---
+++ static4/src/static4/static4.java
@@ -0,0 +1,14 @@
+package static4;
+//http://www.javatpoint.com/static-keyword-in-java
+class student{
+	static{
+		System.out.println("static block");
+	}
+}
+public class static4 {
+	public static void main(String[] args) {
+		System.out.println("hello main");
+		System.exit(0);
+	}
+
+}
